87e1192e2d9d9679d23a3a9524121ddce0eae4fe
Vinicius Ithalo <viniciusith@yahoo.com>
2025-11-17 23:31:41 -0300

    fixed quickshell path and qmlls

--- modules/quickshell/home.nix
+++ modules/quickshell/home.nix
@@ -2,26 +2,38 @@
   pkgs,
   inputs,
   config,
+  lib,
   ...
-}: {
+}: let
+  quickshellPackage = inputs.quickshell.packages.${pkgs.stdenv.hostPlatform.system}.default.override {
+    withJemalloc = true;
+    withQtSvg = true;
+    withWayland = true;
+    withX11 = false;
+    withPipewire = true;
+    withPam = true;
+    withHyprland = false;
+    withI3 = false;
+  };
+in {
   home.packages = with pkgs; [
     qt6Packages.qt5compat
     libsForQt5.qt5.qtgraphicaleffects
     kdePackages.qtbase
     kdePackages.qtdeclarative
     kdePackages.qtstyleplugin-kvantum
-    (inputs.quickshell.packages.${pkgs.stdenv.hostPlatform.system}.default.override {
-      withQMLLib = true;
-      withJemalloc = true;
-      withQtSvg = true;
-      withWayland = true;
-      withX11 = false;
-      withPipewire = true;
-      withPam = true;
-      withHyprland = false;
-      withI3 = false;
-    })
+    quickshellPackage
   ];
+
+  home.sessionVariables = {
+    QML_IMPORT_PATH = lib.makeSearchPath "lib/qt-6/qml" [
+      pkgs.kdePackages.qt5compat
+      pkgs.kdePackages.qtbase
+      pkgs.kdePackages.qtdeclarative
+      pkgs.kdePackages.qtmultimedia
+      quickshellPackage
+    ];
+  };
 
   xdg.configFile."quickshell" = {
     source = config.lib.file.mkOutOfStoreSymlink "/home/viniciusith/dotfiles/config/quickshell";
